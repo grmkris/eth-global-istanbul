c86f43ea794b691540245b708bdb8ce9de9ca7ce
Smuu <18609909+Smuu@users.noreply.github.com>
2023-11-18 01:47:33 +0300

    feat: cow
    Signed-off-by: Smuu <18609909+Smuu@users.noreply.github.com>

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -42,6 +42,15 @@ const landingRoute = new Route({
   },
 });
 
+// cow poc page route
+const CowPocRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/cow-poc",
+  component: () => {
+    return <CoWpoc />;
+  },
+});
+
 // payment page
 const paymentRoute = new Route({
   getParentRoute: () => rootRoute,
@@ -56,6 +65,7 @@ const paymentRoute = new Route({
 // Create the route tree using your root and dynamically generated entity routes
 const routeTree = rootRoute.addChildren([
   landingRoute,
+  CowPocRoute,
   indexRootRoute,
   paymentRoute,
   ...generateAppConfig({ rootRoute }),
@@ -73,3 +83,109 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </TrpcProvider>
   </StrictMode>,
 );
+
+import { SigningScheme, SigningResult, OrderBookApi, OrderQuoteSideKindBuy, OrderSigningUtils, SupportedChainId, OrderParameters, UnsignedOrder, OrderKind, OrderCreation } from '@cowprotocol/cow-sdk'
+import { Web3Provider } from '@ethersproject/providers'
+import React, { useState, useEffect } from 'react';
+
+declare global {
+    interface Window {
+        ethereum: any;
+    }
+}
+
+const account = '0x40D73aa5cA202c7c751F71E158BdAb30Eab7347D'
+const chainId = 5 // Goerli
+const provider = new Web3Provider(window.ethereum)
+const signer = provider.getSigner()
+
+const quoteRequest = {
+    sellToken: '0x91056D4A53E1faa1A84306D4deAEc71085394bC8', // COW goerli - 18 decimals
+    buyToken: '0x07865c6E87B9F70255377e024ace6630C1Eaa37F', // USDC goerli - 6 decimals
+    from: account,
+    receiver: account,
+    buyAmountAfterFee: (10000 * 10 ** 6).toString(), // 10000 USDC
+    kind: OrderQuoteSideKindBuy.BUY,
+}
+
+const orderBookApi = new OrderBookApi({ chainId: SupportedChainId.GOERLI })
+
+export const CoWpoc = () => {
+    const [quote, setQuote] = useState<OrderParameters | null>(null);
+    const [orderSigningResult, setOrderSigningResult] = useState<SigningResult | null>(null);
+    const [order, setOrder] = useState<any | null>(null);
+
+    useEffect(() => {
+        const fetchQuote = async () => {
+            const { quote } = await orderBookApi.getQuote(quoteRequest);
+            setQuote(quote);
+        };
+        fetchQuote();
+    }, []);
+
+    const signOrder = async () => {
+        if (quote) {
+            const usignedOrder:UnsignedOrder = {
+                sellToken: quoteRequest.sellToken,
+                buyToken: quoteRequest.buyToken,
+                receiver: quoteRequest.receiver,
+                kind: OrderKind.BUY,
+                sellAmount: quote.sellAmount,
+                buyAmount: quote.buyAmount,
+                validTo: quote.validTo,
+                appData: quote.appData,
+                feeAmount: quote.feeAmount,
+                partiallyFillable: quote.partiallyFillable,
+
+            }
+            const orderSigningResult = await OrderSigningUtils.signOrder(
+                usignedOrder,
+                chainId,
+                signer
+            );
+            setOrderSigningResult(orderSigningResult);
+        }
+    };
+
+    const sendOrder = async () => {
+        if (quote && orderSigningResult) {
+            const orderCreation:OrderCreation = {
+                sellToken: quoteRequest.sellToken,
+                buyToken: quoteRequest.buyToken,
+                sellAmount: quote.sellAmount,
+                buyAmount: quote.buyAmount,
+                validTo: quote.validTo,
+                kind: OrderKind.BUY,
+                feeAmount: quote.feeAmount,
+                partiallyFillable: quote.partiallyFillable,
+                signingScheme: SigningScheme.EIP712,
+                signature: orderSigningResult.signature,
+                appData: "0x0629b57f996d59b7a06b041e712d9f55033ce042795a08513b5a0aa9e8355966" // {"appCode":"dStripe","metadata":{"hooks":{"version":"0.1.0"}},"version":"0.10.0"}
+            }
+            console.log("Order", orderCreation)
+            const orderId = await orderBookApi.sendOrder(orderCreation);
+            console.log("Order ID", orderId)
+            const order = await orderBookApi.getOrder(orderId);
+            console.log("Order", JSON.stringify(order, null, 2))
+            setOrder(order);
+        }
+    };
+
+    if (!quote) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <>
+            Hello
+            <pre>{JSON.stringify(quote, null, 2)}</pre>
+            <pre>{JSON.stringify(orderSigningResult, null, 2)}</pre>
+            <pre>{JSON.stringify(order, null, 2)}</pre>
+            {orderSigningResult ? (
+                <button onClick={() => sendOrder()}>Send Order</button>
+            ) : (
+                <button onClick={() => signOrder()}>Sign Order</button>
+            )}
+        </>
+    );
+}
