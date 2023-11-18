4f332b690768fa1be5da9a0b7a6503e63fa181b2
Evelin Zirakashvili <zirakashvilievelin@gmail.com>
2023-11-18 14:33:46 +0400

    update invoice

--- apps/frontend/src/features/Invoice.tsx
+++ apps/frontend/src/features/Invoice.tsx
@@ -5,16 +5,16 @@ import {useQuery} from "@tanstack/react-query";
 import {ENABLED_TOKENS_GOERLI, getWalletBalance} from "@/features/balance-check.ts";
 import {useAccount} from "wagmi";
 import {Button} from "@/components/ui/button.tsx";
-import {ArrowLeft} from "lucide-react";
+import {CornerUpLeft} from "lucide-react";
 import {useRouter} from "@tanstack/react-router";
 
 function ConnectButton() {
-    return <w3m-button />
+    return <w3m-button/>
 }
 
-export const Item = (props: {title: string, value: string | number, key: string | number, className?: string}) => {
+export const Item = (props: {title: string, value: string | number, key: string | number}) => {
     return (
-        <div className={`flex items-center ${props.className}`}>
+        <div className="flex items-center">
             <h6 className="text-success-400 text-base min-w-[110px]">{props.title}:</h6>
             <p className="text-base text-success-400 font-bold">{props.value}</p>
         </div>
@@ -22,50 +22,87 @@ export const Item = (props: {title: string, value: string | number, key: string 
 }
 
 export const Invoice = (props: { invoice: selectInvoiceSchema }) => {
-  console.log(props.invoice);
-    const router = useRouter();
+    console.log(props.invoice);
+    const router = useRouter()
 
-  const list = [
-    {name: "ID", value: props.invoice.id},
-    {name: "Description", value: props.invoice.description},
-    {name: "Payer Email", value: props.invoice?.payerEmail ?? ""},
-    {name: "Payer Name", value: props.invoice?.payerName ?? ""},
-    {name: "Payer Wallet", value: props.invoice?.payerWallet ?? ""},
-    {name: "Amount", value: props.invoice?.amountDue + " " +  props.invoice.currency ?? 0},
-    {name: "Wallet", value: props.invoice.wallet},
-    {name: "Status", value: props.invoice.status},
-    {name: "Due date", value: props.invoice?.dueDate?.toLocaleString() ?? ""},
-  ]
+    const list = [
+        {name: "ID", value: props.invoice.id},
+        {name: "Description", value: props.invoice.description},
+        {name: "Payer Email", value: props.invoice?.payerEmail ?? ""},
+        {name: "Payer Name", value: props.invoice?.payerName ?? ""},
+        {name: "Payer Wallet", value: props.invoice?.payerWallet ?? ""},
+        {name: "Amount", value: props.invoice?.amountDue ?? 0},
+        {name: "Currency", value: props.invoice.currency ?? ""},
+        {name: "Wallet", value: props.invoice.wallet},
+        {name: "Status", value: props.invoice.status},
+        {name: "Due date", value: props.invoice?.dueDate?.toLocaleString() ?? ""},
+    ]
+    return (
+        <>
+            <Button
+                variant="dark"
+                className="text-success-400 gap-3 mt-3 ml-2"
+                onClick={() => router.navigate({to: `/invoices`})}
+            >
+                <CornerUpLeft size="16" />
+                Go Back
+            </Button>
+            <div className="max-w-xl p-6 rounded-xl mx-auto mt-20 bg-black  border-success-400">
+                <div className="flex justify-center mb-8">
+                    <QRCode
+                        size={300}
+                        bgColor="bg-primary-900"
+                        fgColor="#9dfc7c"
+                        qrStyle="dots"
+                        eyeRadius={50}
+                        value={`https://metamask.app.link/send/${props.invoice.payerWallet}@05/transfer?address=${props.invoice.wallet}&uint256=${formatUnits(BigInt(props.invoice.amountDue), 6)}`}/>
+                </div>
+                <>
+                    {list.map(i => {
+                        return <Item key={Math.random()} title={i.name} value={i.value}/>
+                    })}
+                </>
+                <div className="flex justify-center mt-8">
+                    <Web3Connect/>
+                </div>
+            </div>
+        </>
 
-  return (
-      <>
-          <Button
-              variant="dark"
-              className="text-success-400 gap-2 ml-2 mt-3"
-              onClick={() => router.navigate({to: `/invoices`})}
-          >
-              <ArrowLeft />
-              Go Back
-          </Button>
-          <div className="max-w-xl p-6 rounded-xl mx-auto mt-20 bg-black  border-success-400">
-              <div className="flex justify-center mb-8">
-                  <QRCode
-                      size={300}
-                      bgColor="bg-primary-900"
-                      fgColor="#9dfc7c"
-                      qrStyle="dots"
-                      eyeRadius={50}
-                      value={`https://metamask.app.link/send/${props.invoice.payerWallet}@05/transfer?address=${props.invoice.wallet}&uint256=${formatUnits(BigInt(props.invoice.amountDue), 6)}`} />
-              </div>
-              <>
-                  {list.map(i => {
-                      return <Item key={Math.random()} title={i.name} value={i.value}/>
-                  })}
-              </>
-              <div className="flex justify-center mt-8">
-                  <Web3Connect/>
-              </div>
-          </div>
-      </>
-  );
+    );
 };
+
+export const Web3Connect = () => {
+    const account = useAccount()
+    const balances = useGetBalances({address: account.address});
+
+    console.log(balances.data);
+    return <ConnectButton/>
+}
+
+
+export const useGetBalances = (props: {
+    address?: Address;
+}) => {
+    return useQuery({
+        enabled: !!props.address,
+        queryKey: ["balances", props.address, ENABLED_TOKENS_GOERLI],
+        queryFn: async () => {
+            if (!props.address) return;
+            const balances = ENABLED_TOKENS_GOERLI.map(async (token) => {
+                if (!props.address) return;
+                const balance = await getWalletBalance({wallet: props.address, erc20: token.address});
+                console.log(balance);
+                return {
+                    token,
+                    balance
+                }
+            })
+
+            // await Promise.all(balances);
+            const data = await Promise.all(balances);
+
+            // filter out empty balances
+            return data.filter(balance => balance?.balance !== BigInt(0));
+        },
+    });
+}
