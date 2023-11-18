b38c4bc7f49dc25cf19ac9f18b078fd9a1babdfd
grmkris <kristjan.grm1@gmail.com>
2023-11-18 17:46:20 +0300

    update clientID
    Signed-off-by: grmkris <kristjan.grm1@gmail.com>

--- apps/frontend/src/features/TrpcProvider.tsx
+++ apps/frontend/src/features/TrpcProvider.tsx
@@ -50,7 +50,7 @@ createWeb3Modal({ wagmiConfig, projectId, chains })
 export function TrpcProvider(props: { children?: React.ReactNode }) {
   const [combinedClient] = useState(() => {
     return trpcClient.createClient({
-      links: [httpBatchLink({ url: "http://localhost:8080/trpc" })],
+      links: [httpBatchLink({ url: "https://back-production-2099.up.railway.app/trpc" })],
     });
   });
 
